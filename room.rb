2c32a7c667d9f75f888f8086c84efd9fac873791
Jen Merritt <jenjomez@gmail.com>
2019-11-30 14:56:15 +0000

    setup room getters and tests passed

--- room.rb
+++ room.rb
@@ -3,11 +3,21 @@
 
 class Room
 
+  attr_reader :name, :capacity
+
   def initialize(name, capacity)
     @name = name
     @capacity = capacity
     @guests = []
     @songs = []
+  end
+
+  def guest_list_length()
+    return @guests.length()
+  end
+
+  def song_list_length()
+    return @songs.length()
   end
 
 end
--- specs/room-spec.rb
+++ specs/room-spec.rb
@@ -12,6 +12,25 @@ require_relative('../guest')
 class RoomTest < MiniTest::Test
 
   def setup
+    @room1 = Room.new("Room 1", 5)
+    @room2 = Room.new("Room 2", 10)
+    @room3 = Room.new("Room 3", 2)
+  end
+
+  def test_can_get_room_name
+    assert_equal("Room 1", @room1.name)
+  end
+
+  def test_can_get_room_capacity
+    assert_equal(5, @room1.capacity)
+  end
+
+  def test_room_guest_list_starts_empty
+    assert_equal(0, @room1.guest_list_length)
+  end
+
+  def test_room_song_list_starts_empty
+    assert_equal(0, @room1.song_list_length)
   end
 
 end
